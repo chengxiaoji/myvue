9ad7b9d5ab2f35b021dcc32bafcf412182220093
chengxiaoji <2572432678@qq.com>
2021-02-19 16:02:14 +0800

    myvue 1.1

---
+++ src/components/test.vue
@@ -0,0 +1,71 @@
+<template>
+<div>
+  <el-row class="demo-avatar demo-basic">
+    <el-col :span="12">
+      <div class="sub-title">circle</div>
+      <div class="demo-basic--circle">
+        <div class="block"><el-avatar :size="50" :src="circleUrl"></el-avatar></div>
+        <div class="block" v-for="size in sizeList" :key="size">
+          <el-avatar :size="size" :src="circleUrl"></el-avatar>
+        </div>
+      </div>
+    </el-col>
+    <el-col :span="12">
+      <div class="sub-title">square</div>
+      <div class="demo-basic--circle">
+        <div class="block"><el-avatar shape="square" :size="50" :src="squareUrl"></el-avatar></div>
+        <div class="block" v-for="size in sizeList" :key="size">
+          <el-avatar shape="square" :size="size" :src="squareUrl"></el-avatar>
+        </div>
+      </div>
+    </el-col>
+  </el-row>
+  <v-carousel v-model="model">
+    <v-carousel-item
+        v-for="(color, i) in colors"
+        :key="color"
+    >
+      <v-sheet
+          :color="color"
+          height="100%"
+          tile
+      >
+        <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+        >
+          <div class="display-3">
+            Slide {{ i + 1 }}
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</div>
+</template>
+
+<script>
+export default {
+name: "test",
+  data () {
+    return {
+      circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
+      squareUrl: "https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png",
+      sizeList: ["large", "medium", "small"],
+      model: 0,
+      colors: [
+        'primary',
+        'secondary',
+        'yellow darken-2',
+        'red',
+        'orange',
+      ],
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/view/home.vue
@@ -0,0 +1,64 @@
+<template>
+<div>
+  <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+  style="min-width: 1100px">
+    <el-menu-item index="1">首页</el-menu-item>
+    <el-menu-item index="2">博客</el-menu-item>
+    <el-menu-item index="3">程序员学院</el-menu-item>
+    <el-menu-item index="4">下载</el-menu-item>
+    <el-menu-item index="5">论坛</el-menu-item>
+    <el-menu-item index="6">代码</el-menu-item>
+    <el-menu-item index="7" style="float: right">管理中心</el-menu-item>
+    <el-menu-item index="8" style="float: right">个人中心</el-menu-item>
+
+    <el-menu-item index="9" style="float: right">
+
+
+
+        <el-dropdown>
+  <span class="el-dropdown-link">
+    <el-avatar :size="40" src="https://empty" @error="errorHandler" ></el-avatar>
+  </span>
+<!--          下拉菜单<i class="el-icon-arrow-down el-icon&#45;&#45;right"></i>-->
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>黄金糕</el-dropdown-item>
+            <el-dropdown-item>狮子头</el-dropdown-item>
+            <el-dropdown-item>螺蛳粉</el-dropdown-item>
+            <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+            <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+    </el-menu-item>
+
+    <el-input
+        class="searchinput"
+        placeholder="请输入内容"
+        prefix-icon="el-icon-search"
+        v-model="input2">
+    </el-input>
+
+  </el-menu>
+</div>
+</template>
+
+<script>
+export default {
+  name: "home"
+}
+</script>
+
+<style scoped>
+.searchinput{
+  float: right;
+  width: 260px;
+  margin-top: 10px;
+  margin-right: 66px;
+}
+</style>
